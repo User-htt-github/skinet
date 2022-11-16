61451576702a4275b3e841549591e84e293c01c3
Trần Tuấn Hùng <hungtt@kaopiz.com>
2022-11-16 14:32:19 +0700

    Initial commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,12 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        // * Product id
+        public int Id { get; set; }
+
+        // * Product name
+        public string Name { get; set; }
+
+    }
+}
