8e19874b9e25dd3fe1c843d1d6c20c9c1d4c2f53
User-htt-github <118340304+User-htt-github@users.noreply.github.com>
2023-06-13 09:44:25 +0700

    section 12

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -10,18 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
-  
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+    this.bcService.set('@productDetails', ' ')
+  }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
-  loadProduct(){
+  loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
-      error: error => console.log(error) 
+      next: product => {
+        this.product = product;
+        this.bcService.set('@productDetails', product.name)
+      },
+      error: error => console.log(error)
     });
   }
 }
