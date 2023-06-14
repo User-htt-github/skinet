77f4a6e445ce4b42cb65d1fb48c04bd568c64bfc
User-htt-github <118340304+User-htt-github@users.noreply.github.com>
2023-06-14 14:30:37 +0700

    section 13

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -3,6 +3,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -14,21 +15,30 @@ namespace API.Extensions
         {
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
-            services.AddDbContext<StoreContext>(opt => {
+            services.AddDbContext<StoreContext>(opt =>
+            {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
-
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.Configure<ApiBehaviorOptions>(options => {
-                options.InvalidModelStateResponseFactory = actionContext => {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
                     var errors = actionContext.ModelState
                             .Where(e => e.Value.Errors.Count > 0)
                             .SelectMany(x => x.Value.Errors)
                             .Select(x => x.ErrorMessage).ToArray();
 
-                    var errorResponse = new ApiValidationErrorResponse {
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
                         Errors = errors
                     };
 
@@ -37,8 +47,10 @@ namespace API.Extensions
             });
 
             services.AddCors(
-                options => {
-                    options.AddPolicy("CorsPolicy", policy => {
+                options =>
+                {
+                    options.AddPolicy("CorsPolicy", policy =>
+                    {
                         policy
                             .AllowAnyHeader()
                             .AllowAnyMethod()
